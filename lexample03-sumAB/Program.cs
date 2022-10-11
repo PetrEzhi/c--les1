79a4014f3379ec9ada68452c3c7238ac9fbc32f0
P E <bra811@mail.ru>
2022-10-11 20:51:13 +0300

    Dobavil random v ex3

--- lexample03-sumAB/Program.cs
+++ lexample03-sumAB/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 13;
-int numberb = 27;
-int sum = numberA + numberb;
+﻿int numberA = new Random().Next(1,30);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,20);
+Console.WriteLine(numberB);
+int sum = numberA + numberB;
 Console.WriteLine(sum);
